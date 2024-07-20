56ac8a67b6a1cde0b066bf43ae3a9ba82ca72757
HEET VEKAIRYA <heetvekariya50@gmail.com>
2024-07-20 22:39:03 +0530

    feat: split text into sentences and words

---
+++ Scripts/spliter.py
@@ -0,0 +1,16 @@
+def splitter(text):
+    # Split the text into sentences
+    sentences = text.split('.')
+    
+    # Make list of individual words
+    words = []
+    for sentence in sentences:
+        words.extend(sentence.split())
+
+    return sentences, words
+
+if __name__ == "__main__":
+    text = "This is a sample sentence. This is another sample sentence."
+    sentences, words = splitter(text)
+    print(sentences)
+    print(words)
